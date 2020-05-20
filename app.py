36d5ffa9b2ee126ccbbeca1e4c1815b516a88696
Ubuntu <twong@twtest.lhdx5o2q0cfujbl3czxm3zpspd.gx.internal.cloudapp.net>
2020-05-20 14:42:04 +0000

    fixed linter and linting issues

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -68,4 +68,4 @@ def predict():
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=8080, debug=True) # specify port=80
